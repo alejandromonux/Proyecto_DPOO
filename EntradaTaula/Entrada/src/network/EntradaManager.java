80bb554eb6d5782b4330a2e18e5c83bbd7eb3c19
JCarri14 <joancarrion14@gmail.com>
2019-05-18 17:46:48 +0200

    #12 Fix: .idea to commit

--- EntradaTaula/Entrada/src/network/EntradaManager.java
+++ EntradaTaula/Entrada/src/network/EntradaManager.java
@@ -29,7 +29,7 @@ public class EntradaManager extends Thread {
         PORT = config.getPort_Entrada();
         socket = new Socket(IP, PORT);
         dis = new DataInputStream(socket.getInputStream());
-        //ois = new ObjectInputStream(socket.getInputStream());
+        ois = new ObjectInputStream(socket.getInputStream());
         dos = new DataOutputStream(socket.getOutputStream());
     }
 
@@ -106,10 +106,10 @@ public class EntradaManager extends Thread {
                 boolean done = dis.readBoolean();
                 if (!done){
                     controller.insertNotification();
+                } else {
+                    controller.updateRequestList();
                 }
-
                 break;
-
         }
 
     }
