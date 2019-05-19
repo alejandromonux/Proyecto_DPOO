22be70a811854abc9c8f725a4af0372fee3ab767
MarcAynes <marcaynes2@gmail.com>
2019-05-19 14:29:48 +0200

    quien toque codigo de entrada le rajo que ya funciona

--- AT2/src/main/java/com/dpo/centralized_restaurant/Network/ServerEntrada.java
+++ AT2/src/main/java/com/dpo/centralized_restaurant/Network/ServerEntrada.java
@@ -20,6 +20,7 @@ public class ServerEntrada extends Thread {
     private int PORT;
     private ServerSocket serverSocket;
     private final ArrayList<DedicatedServerEntrada> dedicatedServers;
+    private DedicatedServerEntrada dedicatedServer;
     private RequestManager requestsManager;
     private ConectorDB conectorDB;
     private Controller controller;
@@ -41,13 +42,27 @@ public class ServerEntrada extends Thread {
             isRunning = true;
 
             while (isRunning) {
+                System.out.println("ruuuuuuun boiii 2");
                 if(dedicatedServers.isEmpty()){
+                    System.out.println(isRunning + " is running1");
                     Socket socket = serverSocket.accept();  // Esperem a que algun usuari es connecti
+                    System.out.println(isRunning + " is running2");
                     System.out.println("Connected");
                     //Modified by: Marc --> Added dedicatedServers in constructor
                     DedicatedServerEntrada dServer = new DedicatedServerEntrada(socket, requestsManager, dedicatedServers, conectorDB, controller);   // Creem un cami dedicat a la connexio amb aquest usuari
+                    System.out.println(isRunning + " is running3");
                     dedicatedServers.add(dServer);
+                    dedicatedServer = dServer;
+                    System.out.println(isRunning + " is running4");
+                    if  (dedicatedServers.isEmpty()){
+                        System.out.println("empty - running");
+                    }else{
+                        System.out.println("no empty - running");
+                    }
+                    System.out.println(isRunning + " is running5");
                     dServer.start();
+                    System.out.println("uala chuta");
+                    System.out.println(isRunning + " is running");
                 }
             }
 
@@ -55,6 +70,7 @@ public class ServerEntrada extends Thread {
         } finally {
             if (serverSocket != null && !serverSocket.isClosed()) {
                 try {
+                    System.out.println("clouseo");
                     serverSocket.close();
                 } catch (IOException e) {
                     e.printStackTrace();
@@ -68,9 +84,22 @@ public class ServerEntrada extends Thread {
      */
     public void closeServer(){
         isRunning = false;
-        dedicatedServers.get(0).closeDedicatedServer();
-
+        //this.stop();
         try {
+            serverSocket.close();
+            System.out.println("close");
+        } catch (IOException e) {
+
+        }
+        System.out.println("closeServer()1");
+        dedicatedServers.get(0).closeDedicatedServer();
+        System.out.println("closeServer()2");
+        dedicatedServer.closeDedicatedServer();
+        System.out.println("clouseo server");
+        dedicatedServer = null;
+        dedicatedServers.remove(0);
+        try {
+            System.out.println("clouseo 2");
             serverSocket.close();
         } catch (IOException e) {
             e.printStackTrace();
@@ -83,11 +112,14 @@ public class ServerEntrada extends Thread {
      */
     public void updateAssignment(Request nuevoRequest){
         try {
-            dedicatedServers.get(0).sendPass(nuevoRequest);
+            if  (dedicatedServers.isEmpty()){
+                System.out.println("empty");
+            }
+            dedicatedServer.sendPass(nuevoRequest);
         }catch (NullPointerException e){
-
+            System.out.println("null pointer");
         }catch (IndexOutOfBoundsException e1){
-
+            System.out.println("index out of");
         }
     }
 
